eb7f0a3107362900ab4ba27232b192a8b5d4f7ef
Dimitrije Stamenic <41240101+stamd@users.noreply.github.com>
2022-05-26 17:07:15 +0200

    Update Py File

--- src/OMM_Seminarski-01.py
+++ src/OMM_Seminarski-01.py
@@ -1,30 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import matplotlib.pyplot as plt
 import math
 import numpy as np
 
-
-# In[2]:
-
-
 def izracunajA(a, b, A0, B0, t):
     return ((math.sqrt(a*b)*A0 - a*B0)/(2*math.sqrt(a*b))*(math.e**(math.sqrt(a*b)*t))) + ((math.sqrt(a*b)*A0 + a*B0)/(2*math.sqrt(a*b))*(math.e**(-math.sqrt(a*b)*t)))
 
-
-# In[3]:
-
-
 def izracunajB(a, b, A0, B0, t):
     return ((math.sqrt(a*b)*B0 - b*A0)/(2*math.sqrt(a*b))*(math.e**(math.sqrt(a*b)*t))) + ((math.sqrt(a*b)*B0 + b*A0)/(2*math.sqrt(a*b))*(math.e**(-math.sqrt(a*b)*t)))
-
-
-# In[4]:
-
 
 def odrediTrenutakPobede(a, b, A0, B0):
     for i in list(odrediInterval(0, 100, 0.01)):
@@ -32,20 +14,12 @@ def odrediTrenutakPobede(a, b, A0, B0):
             return i-0.00001
     return i
 
-
-# In[5]:
-
-
 def odrediInterval(pocetak, kraj, korak):
     interval = []
     while pocetak < kraj:
         interval.append(pocetak)
         pocetak += korak
     return interval
-
-
-# In[6]:
-
 
 def simulirajBitku(a, b, A0, B0):
     trenutakPobede = odrediTrenutakPobede(a, b, A0, B0)
@@ -55,12 +29,14 @@ def simulirajBitku(a, b, A0, B0):
     
     plt.plot(t, A, label='Vojska A')
     plt.plot(t, B, label='Vojska B')
+    
+    informacije = "A0=" + str(A0) + "\nB0=" + str(B0)+ "\na=" + str(a) + "\nb=" + str(b)
+    plt.plot([], [], ' ', label=informacije)
 
-    plt.legend(loc='upper right')
+    plt.legend()
     plt.xlabel('Vreme t')
-    plt.ylabel('brojnost vojske')
+    plt.ylabel('Brojnost vojske')
 
-    bottom, top = plt.ylim()
     plt.show()
 
     if (b*A0**2 > a*B0**2):
@@ -70,21 +46,9 @@ def simulirajBitku(a, b, A0, B0):
     else:
         print("Neodlucna bitka!")
 
+simulirajBitku(0.5, 1, 500, 700)
 
-# In[7]:
-
-
-a = 1
-b = 2
-A0 = 500
-B0 = 700
-simulirajBitku(a, b, A0, B0)
-
-
-# Sledeca funkcija ispituje ko pobedjuje u zavisnosti od ubojitosti - mi joj dajemo brojnost naste i protivnicke vojske, a ona nam vraca predvidjanje pobede u zavisnosti od mogucih koeficijenata ubojitosti
-
-# In[8]:
-
+simulirajBitku(1, 1, 1400, 700)
 
 def odrediPobednika(a, b, A0, B0):
     if (b*A0**2 > a*B0**2):
@@ -99,25 +63,18 @@ def simulirajBitku2(A0, B0):
     r = np.array(list(range(N)))
     pobednici = np.zeros((N, N))
     
-# za svaki x y nadjemo ko je pobedio
-# onaj ko je pobedio -> koord idu u odg niz
-# nasa ubojitost b
+    # x je ustvari nasa ubojitost (b)
     for x in r:
-        # protivnicka ubojitost a
+        # protivnicka ubojitost (a)
         for y in r:
             if x==0 or y==0:
                 continue
             w = odrediPobednika(y, x, A0, B0)
             if (w == "A"):
-                # winnerA[x] = y
                 pobednici[y][x] = 1
-                # winnerA[x].append(y)
             elif (w == "B"):
-                # winnerB[x] = y
                 pobednici[y][x] = -1
-            # else:
-                # drawn[x] = y
-    
+                
     noviPobednici = np.delete(pobednici, 0, 0)
     noviPobednici = np.delete(noviPobednici, 0, 1)
 
@@ -125,20 +82,45 @@ def simulirajBitku2(A0, B0):
     plt.xlabel('Ubojitost vojske A')
     plt.ylabel('Ubojitost vojske B')
 
-    plt.colorbar()
+    cbar = plt.colorbar()
+    cbar.set_ticks([-1,0,1])
+    cbar.set_ticklabels(["Pobeda B","Nereseno","Pobeda A"])
     plt.show()
 
-
-# In[9]:
-
-
 simulirajBitku2(500, 500)
-
-
-# Ljubicasta oznacava pobedu protivnika (vojske B), zuta nasu pobedu (vojske A) a ova zelenkasta je nereseno
-
-# In[10]:
-
-
 simulirajBitku2(500, 1000)
 
+def izracunajZasedaA(a, b, t, d1, c2):
+    return (2*d1)/(c2*(math.e**(d1*b*t)) - 1) + d1   
+
+def izracunajZasedaB(a, b, t, d1, c2):
+    return (2/a)*((b*c2*(d1**2)*(math.e**(d1*b*t)))/((c2*(math.e**(d1*b*t)) - 1)**2))
+
+def nadjiC2(a, b, A0, B0):
+    a1 = B0
+    b1 = 2*B0 - (2*b*(A0**2))/a
+    c1 = B0
+    return (-b1 + math.sqrt((b1**2) - 4*a1*c1))/(2*a1) 
+
+def simulirajBitkuZaseda(a, b, A0, B0):
+    c2 = nadjiC2(a, b, A0, B0)
+    d1 = A0 * ((c2-1)/(c2+1))
+    trenutakPobede = odrediTrenutakPobede(a, b, A0, B0)
+    t = np.array(list(odrediInterval(0, trenutakPobede, 0.001)))
+    A = izracunajZasedaA(a, b, t, d1, c2)
+    B = izracunajZasedaB(a, b, t, d1, c2)
+    
+    plt.plot(t, A, label='Vojska A')
+    plt.plot(t, B, label='Vojska B')
+
+    informacije = "A0=" + str(A0) + "\nB0=" + str(B0)+ "\na=" + str(a) + "\nb=" + str(b)
+    plt.plot([], [], ' ', label=informacije)
+    
+    plt.legend(loc='upper right')
+    plt.xlabel('Vreme t')
+    plt.ylabel('Brojnost vojske')
+
+    plt.show()
+
+simulirajBitkuZaseda(1, 0.6, 100, 150)
+simulirajBitkuZaseda(1, 1, 200, 100)
